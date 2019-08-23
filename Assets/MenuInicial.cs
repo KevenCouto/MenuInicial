4b20e7c4b8519a087d7dfb8ee0be9904ed5e8a9f
33316@ETECAF.LOCAL <33316@ETECAF.LOCAL>
2019-08-23 16:55:43 -0300

    menu

---
+++ Assets/MenuInicial.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+public class MenuInicial : MonoBehaviour
+{
+    public GameObject panel1;
+    public GameObject panel2;
+    public void start()
+    {
+        SceneManager.LoadScene("C1");
+    }
+    public void settings()
+    {
+        panel1.SetActive(false);
+        panel2.SetActive(true);
+    }
+    public void quit()
+    {
+        Application.Quit();
+    }
+}
---
+++ Assets/settinsg.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class settinsg : MonoBehaviour
+{
+    public GameObject panel1;
+    public GameObject panel2;
+    public void settings()
+    {
+       panel1.SetActive(true);
+        panel2.SetActive(false);
+    }
+}
